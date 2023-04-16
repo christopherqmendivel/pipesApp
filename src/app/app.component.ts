af85817cc24735e1adb8c03245a212ace6646286
Christopher Quiroz <cquirozmendivel@gmail.com>
2023-04-16 15:23:19 +0200

    P class Decimal, Currency y Percent Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  
+  nombre: string = 'christopher quIroz';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Christopher'
+  }
+
+  mostrarNombre(){
+    console.log( this.nombre );
+    console.log( this.valor );
+    console.log( this.obj );
+  }
 }
---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+
+//import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
+
+// PrimeNG
+
+
+@NgModule({
+  exports: [
+    //MenuModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule
+    
+  ]
+  
+})
+export class PrimeNgModule { }
